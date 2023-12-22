90b83ddd7d40b2e653e6d039640d491afa7d1d46
moszer <moszergamemer777@gmail.com>
2023-12-22 23:41:42 +0700

    fix rount page not found

--- src/main.jsx
+++ src/main.jsx
@@ -1,58 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Designs from './components/Designs/Designs.jsx'
-import './index.css'
-import Home from './components/Home/Home.jsx'
-import Login from './components/login-register/login.jsx'
-import Register from './components/login-register/register.jsx'
-import Checkout from './components/checkout/Checkout.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import Designs from './components/Designs/Designs.jsx';
+import './index.css';
+import Home from './components/Home/Home.jsx';
+import Login from './components/login-register/Login.jsx'; // แก้ชื่อไฟล์
+import Register from './components/login-register/Register.jsx';
+import Checkout from './components/checkout/Checkout.jsx';
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
-
-import {
-  RecoilRoot,
-} from 'recoil';
-
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
   },
   {
-    path: "/Designs",
-    element: <Designs />
+    path: '/Designs',
+    element: <Designs />,
   },
   {
-    path: "/Home",
-    element: <Home />
+    path: '/Home',
+    element: <Home />,
   },
   {
-    path: "/Login",
-    element: <Login />
+    path: '/Login',
+    element: <Login />,
   },
   {
-    path: "/Register",
-    element: <Register />
+    path: '/Register',
+    element: <Register />,
   },
   {
-    path: "/Checkout",
-    element: <Checkout />
-  }
+    path: '/Checkout',
+    element: <Checkout />,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// createRoot ไม่จำเป็นในกรณีนี้ ใช้ ReactDOM.render แทน
+ReactDOM.render(
   <React.StrictMode>
-     <RecoilRoot>
+    <RecoilRoot>
       <div className="bg-base-200">
-          <RouterProvider router={router} />
+        <RouterProvider router={router} />
       </div>
-     </RecoilRoot>
+    </RecoilRoot>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
